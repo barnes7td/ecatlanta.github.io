07fb824337e4b226056c900a7bfb13b2859f70e5
Timothy Barnes <barnes7td@gmail.com>
2017-07-04 14:21:39 -0400

    Fix form and add back html5mode

--- scripts/controllers/HomeCtrl.js
+++ scripts/controllers/HomeCtrl.js
@@ -1,17 +1,47 @@
 (function() {
-    function HomeCtrl(Attendee) {
+    function HomeCtrl(Attendee, $cookies, $scope) {
         let vm = this;
         
         vm.attendees = Attendee.all;
+        vm.inFormMode = false;
+        vm.newAttendee = {};
+        
+        init();
+        
+        vm.switchRSVPForm = function() {
+            let registered = $cookies.get("ecatlanta2017registered");
+            console.log(registered);
+            registered ? vm.isRegistered = true : null;
+            
+            vm.inFormMode = !vm.inFormMode;
+        }
         
         vm.addAttendee = function() {
-            console.log("add");
+            vm.newAttendee.arrivalDate = vm.newAttendee.arrivalDate.getTime();
+            vm.newAttendee.departureDate = vm.newAttendee.departureDate.getTime();
+            console.log(vm.newAttendee);
+            
             Attendee.add(vm.newAttendee);
+            $cookies.put("ecatlanta2017registered", true);
+            vm.newAttendee = "";
+            vm.inFormMode = false;
+            vm.isRegistered = true;
+        }
+        
+        function init() {
+            checkCookiesForRSVP();
+        }
+        
+        function checkCookiesForRSVP() {
+            let registered = $cookies.get("ecatlanta2017registered");
+            registered ? vm.isRegistered = true : vm.isRegistered = false;
+            console.log(vm.isRegistered);
+            // debugger;
         }
         
     }
 
     angular
         .module('ECAtlSite')
-        .controller('HomeCtrl', ['Attendee', HomeCtrl]);
+        .controller('HomeCtrl', ['Attendee', '$cookies', '$scope', HomeCtrl]);
 })();
